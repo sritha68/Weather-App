519e864c0f11bc774e7836952f8aac0502c6dce4
Sritha Ch <sritha0608@gmail.com>
2025-11-20 17:58:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,14 +43,40 @@ function changeBackground(weather) {
     const body = document.body;
 
     if (weather.includes("cloud")) {
-        body.style.background = "url('cloudy.avif') no-repeat center/cover";
+        body.style.background = "linear-gradient(135deg, #757f9a, #d7dde8)";
+        changeAnimation("https://lottie.host/dd30b5fb-d52c-4bdf-ae8c-ee702ac96318/AGKZZGoVyV.json");
+
     } else if (weather.includes("rain")) {
-        body.style.background = "url('rain.jpg') no-repeat center/cover";
+        body.style.background = "linear-gradient(135deg, #4b79a1, #283e51)";
+        changeAnimation("https://lottie.host/69eac07f-d0df-4740-a00a-3bf612046fd0/dYPj8CnObP.json");
+
     } else if (weather.includes("clear")) {
-        body.style.background = "url('clear.jpg') no-repeat center/cover";
+        body.style.background = "linear-gradient(135deg, #56ccf2, #2f80ed)";
+        changeAnimation("https://lottie.host/0b390c71-4a59-4427-9c76-c2a18205a01c/8CwSmzNtq8.json");
+
     } else if (weather.includes("snow")) {
-        body.style.background = "url('snow.jpg') no-repeat center/cover";
+        body.style.background = "linear-gradient(135deg, #83a4d4, #b6fbff)";
+        changeAnimation("https://lottie.host/d8064e61-2558-4032-a80d-8b76abbe5f20/LpV1Ayzf7o.json");
+
     } else {
         body.style.background = "linear-gradient(135deg, #4e54c8, #8f94fb)";
+        changeAnimation("https://lottie.host/dd30b5fb-d52c-4bdf-ae8c-ee702ac96318/AGKZZGoVyV.json");
     }
+}
+let animation = null;
+
+function changeAnimation(animationURL) {
+    const container = document.getElementById("weatherAnimation");
+
+    if (animation) {
+        animation.destroy(); // Remove previous animation
+    }
+
+    animation = lottie.loadAnimation({
+        container: container,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        path: animationURL
+    });
 }
